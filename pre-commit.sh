541a9e63b168f3506e2e04dcd6ab3c6b17740804
tikendraw <tikendraksahu1029@gmail.com>
2023-08-10 16:38:32 +0530

    first commit

---
+++ pre-commit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#black .
+
+# If you want to format Python files only, you can use the following code instead:
+#
+black -l 80 .
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version="0.1.0",
+    description="This projects recommends Movies based of Users interest through collabing with other users and their choices.",
+    author="Tikendra",
+    license="",
+)
