f2c360bffc7a500b1d0f4b4bbcca74b64c1d5a6e
tikendraw <tikendraksahu1029@gmail.com>
2023-08-18 07:00:58 +0530

    updated

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+import os
+import importlib
+
+# Get the current directory of this package
+package_directory = os.path.dirname(__file__)
+
+# List all files in the package directory
+package_files = [f for f in os.listdir(package_directory) if f.endswith(".py") and f != "__init__.py"]
+
+# Import each module dynamically
+for file_name in package_files:
+    module_name = file_name[:-3]  # Remove the ".py" extension
+    module_path = f"{__name__}.{module_name}"  # Construct the full module path
+    importlib.import_module(module_path)
+
+# Optionally, you can provide a list of module names to __all__ for better organization
+__all__ = package_files
